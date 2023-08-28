18d6dee831edea764380c62ea87512b4665aa8c5
SeungWon Jung <jung946244@naver.com>
2023-08-28 12:35:50 +0900

    오전에 공부한 내용

---
+++ src/ch05reference/C11method.java
@@ -0,0 +1,21 @@
+package ch05reference;
+
+public class C11method {
+	public static void main(String[] args) {
+		int[] a = method1();
+		
+		System.out.println("main 내부");
+		System.out.println(System.identityHashCode(a));
+		
+	}
+
+	public static void method1() {
+		int[] c = { 9, 7, 5 };
+		
+		System.out.println("method1 내부");
+		System.out.println(System.identityHashCode(c));
+		
+		return c;
+	}
+
+}
