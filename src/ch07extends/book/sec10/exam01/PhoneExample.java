cd4e1aa6d3d357f2f6d7e3d5b42803c928d1b2a3
SeungWon Jung <jung946244@naver.com>
2023-08-30 15:31:27 +0900

    8.30 6period abstract exam

--- src/ch07extends/book/sec10/exam01/PhoneExample.java
+++ src/ch07extends/book/sec10/exam01/PhoneExample.java
@@ -4,8 +4,8 @@ public class PhoneExample {
     public static void main(String[] args) {
         SmartPhone smartPhone = new SmartPhone("홍길동");
 
-        smartPhone.turnOn();
+        smartPhone.turnOn();        //Phone의 메서드
         smartPhone.internetSearch();
-        smartPhone.turnOff();
+        smartPhone.turnOff();       //Phone의 메서드
     }
 }
