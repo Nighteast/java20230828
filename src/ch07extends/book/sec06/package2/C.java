cb0e20227f5acb5edfab77bdaf3b0bc0d6e22881
SeungWon Jung <jung946244@naver.com>
2023-08-31 10:12:13 +0900

    8.31 period1 accessModifier sec06

--- src/ch07extends/book/sec06/package2/C.java
+++ src/ch07extends/book/sec06/package2/C.java
@@ -4,8 +4,8 @@ import ch07extends.book.sec06.package1.A;
 
 public class C {
     public void method() {
-        A a = new A();
-        a.field = "value";
-        a.method();
+//        A a = new A();    // 다른 패키지에 있기 때문에 A의 protected 필드, 생성자, 메소드에 접근 불가
+//        a.field = "value";
+//        a.method();
     }
 }
