d87aad966bfe0276413639c0bfcd1e5508f31410
SeungWon Jung <jung946244@naver.com>
2023-08-31 09:48:22 +0900

    8.31 period1 accessModifier

---
+++ src/ch07extends/lecture/MyClass30.java
@@ -0,0 +1,31 @@
+package ch07extends.lecture;
+
+public class MyClass30 {
+    private void privateMethod() {
+        System.out.println("MyClass30.privateMethod");
+    }
+
+    public void publicMethod() {
+        System.out.println("MyClass30.publicMethod");
+
+        //private 멤버(필드, 생성자, 메서드) 접근 가능
+        privateMethod();
+    }
+
+    void packagePrivateMethod() {
+        System.out.println("MyClass30.packagePrivateMethod");
+
+        //private 멤버 접근 가능
+        privateMethod();
+
+
+    }
+
+    protected void protectedMethod() {
+        System.out.println("MyClass30.protectedMethod");
+
+        privateMethod();
+        packagePrivateMethod();
+        publicMethod();
+    }
+}
