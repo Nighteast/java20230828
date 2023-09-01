17a776dc20bebdaa895eb9a84f0bcfd58f7c50ad
SeungWon Jung <jung946244@naver.com>
2023-09-01 15:24:25 +0900

    9.01 period6

---
+++ src/ch09nested/lecture/C03importStatic.java
@@ -0,0 +1,18 @@
+package ch09nested.lecture;
+
+//import static java.lang.Math.random;
+//import static java.lang.Math.PI;
+import static java.lang.Math.*;
+
+public class C03importStatic {
+    public static void main(String[] args) {
+        //잠깐 다른 길로...
+        //import하여 바로 호출하기.
+
+        double random1 = random();
+        double random2 = random();
+        double random3 = random();
+
+        double length = PI * 3 * 2;
+    }
+}
