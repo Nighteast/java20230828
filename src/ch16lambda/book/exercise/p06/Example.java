443b8068ede41986ad8cb0a486844dacb51b47aa
SeungWon Jung <jung946244@naver.com>
2023-09-04 15:27:27 +0900

    9.04 period6 lambda p06

---
+++ src/ch16lambda/book/exercise/p06/Example.java
@@ -0,0 +1,19 @@
+package ch16lambda.book.exercise.p06;
+
+public class Example {
+    @FunctionalInterface
+    interface Function {
+        double apply(double x, double y);
+    }
+
+    public static double calc(Function fun) {
+        double x = 10;
+        double y = 4;
+        return fun.apply(x, y);
+    }
+
+    public static void main(String[] args) {
+        double result = calc((x,y) -> (x/y));
+        System.out.println("result = " + result);
+    }
+}
