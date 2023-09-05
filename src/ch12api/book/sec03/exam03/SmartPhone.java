ccc8beb9285f6d53cae53edda68b6ad3101cea78
SeungWon Jung <jung946244@naver.com>
2023-09-05 11:18:59 +0900

    9.05 period3 api toString

--- src/ch12api/book/sec03/exam03/SmartPhone.java
+++ src/ch12api/book/sec03/exam03/SmartPhone.java
@@ -1,4 +1,19 @@
 package ch12api.book.sec03.exam03;
 
 public class SmartPhone {
+    private String company;
+    private String os;
+
+    public SmartPhone(String company, String os) {
+        this.company = company;
+        this.os = os;
+    }
+
+    @Override
+    public String toString() {
+        return "SmartPhone{" +
+                "company='" + company + '\'' +
+                ", os='" + os + '\'' +
+                '}';
+    }
 }
