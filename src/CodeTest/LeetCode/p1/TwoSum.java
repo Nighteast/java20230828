c8f6f9d1551884e401560a26d4b578ced4828e10
SeungWon Jung <jung946244@naver.com>
2023-09-08 10:22:22 +0900

    9.08 leetCode

---
+++ src/CodeTest/LeetCode/p1/TwoSum.java
@@ -0,0 +1,23 @@
+package CodeTest.LeetCode.p1;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
