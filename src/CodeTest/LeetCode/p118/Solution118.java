af1f9124686bda7813e74293d13e49adad24eb51
SeungWon Jung <jung946244@naver.com>
2023-09-08 15:30:46 +0900

    9.08 period6 nestedList leekCode

--- src/CodeTest/LeetCode/p118/Solution118.java
+++ src/CodeTest/LeetCode/p118/Solution118.java
@@ -8,8 +8,28 @@ public class Solution118 {
         Solution118 o1 = new Solution118();
         o1.generate(5);
     }
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
+
+        list.add(new ArrayList<>());
+        list.get(0).add(1);
+        System.out.println(list);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> preRow = list.get(i - 1);
+
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(preRow.get(j - 1) + preRow.get(j));
+            }
+
+            row.add(1);
+
+            list.add(row);
+        }
 
 
         return list;
