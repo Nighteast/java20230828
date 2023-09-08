3b6c7cc86b7b125dc758a6fb1f377e50996a12f6
SeungWon Jung <jung946244@naver.com>
2023-09-08 14:19:56 +0900

    9.08 period5 nestedList

---
+++ src/CodeTest/LeetCode/p118/Solution118.java
@@ -0,0 +1,17 @@
+package CodeTest.LeetCode.p118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution118 {
+    public static void main(String[] args) {
+        Solution118 o1 = new Solution118();
+        o1.generate(5);
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> list = new ArrayList<>();
+
+
+        return list;
+    }
+}
