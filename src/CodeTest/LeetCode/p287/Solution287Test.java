de1394b8758b5751c32574c7a3ed0d2653c30bdc
SeungWon Jung <jung946244@naver.com>
2023-09-11 10:32:51 +0900

    9.11 period2 LeetCode

---
+++ src/CodeTest/LeetCode/p287/Solution287Test.java
@@ -0,0 +1,18 @@
+package CodeTest.LeetCode.p287;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution287Test {
+    @Test
+    void findDuplicate() {
+        Solution287 o = new Solution287();
+        int num1 = o.findDuplicate(new int[]{1, 3, 4, 2, 2});
+        assertEquals(2, num1);
+
+        int num2 = o.findDuplicate(new int[]{3, 1, 3, 4, 2});
+        assertEquals(3, num2);
+
+    }
+}
