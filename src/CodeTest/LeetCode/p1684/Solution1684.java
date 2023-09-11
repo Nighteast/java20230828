b7306108c0294b552d57e1e0820c1bd2361584d1
SeungWon Jung <jung946244@naver.com>
2023-09-11 11:58:10 +0900

    9.11 LeetCode

---
+++ src/CodeTest/LeetCode/p1684/Solution1684.java
@@ -0,0 +1,27 @@
+package CodeTest.LeetCode.p1684;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution1684 {
+    public int countConsistentStrings(String allowed, String[] words) {
+        Set<Character> allowedSet = new HashSet<>();
+        for (char c : allowed.toCharArray()) {
+            allowedSet.add(c);
+        }
+
+        int count = 0;
+
+        for (String s : words) {
+            int tmp = 1;
+            for (char c : s.toCharArray()) {
+                if (!(allowedSet.contains(c))) {
+                    tmp = 0;
+                }
+            }
+            count += tmp;
+        }
+
+        return count;
+    }
+}
