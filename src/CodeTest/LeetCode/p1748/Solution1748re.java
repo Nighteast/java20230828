3f0c0c1d4525a9d49ef36f37151c64400008330f
SeungWon Jung <jung946244@naver.com>
2023-09-13 16:18:48 +0900

    9.13 stream

--- src/CodeTest/LeetCode/p1748/Solution1748re.java
+++ src/CodeTest/LeetCode/p1748/Solution1748re.java
@@ -1,45 +1,26 @@
 package CodeTest.LeetCode.p1748;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Solution1748re {
     public int sumOfUnique(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
+        // 1, 2, 3, 2
 
-        for (int num : nums) {
-            if (map.containsKey(num)) {
-                // 이미 있으면
-                int oldValue = map.get(num);
-                map.put(num, oldValue + 1);
-            } else {
-                map.put(num, 1); // 첫 발견
-            }
-        }
-
-        // [1, 1}
-        // [2, 2}
-        // [3, 1}
-
-        int sum = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue().equals(1)) {
-                sum += entry.getKey();
-            }
-        }
-
-
-
-//        int result = 0;
-//        Set<Integer> set = new HashSet<>();
-//        for (int i = 0; i < set.size(); i++) {
-//            if(!set.contains(nums[i])){
-//                result += nums[i];
-//            }
-//            set.add(nums[i]);
-//
-//        }
-
-        return sum;
+        // 수 : 몇개 (counting)
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.counting()))
+        // 1개인 entry만 거르고 (filter)
+                .entrySet().stream()
+                .filter(entry -> entry.getValue() == 1)
+        // entry의 키들
+                .map(e->e.getKey())
+        // 만 더하기
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
