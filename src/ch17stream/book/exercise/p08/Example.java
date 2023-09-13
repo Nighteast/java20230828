a648258cb6412b5c62aa311c8c9248baacb08549
SeungWon Jung <jung946244@naver.com>
2023-09-13 14:46:49 +0900

    9.13 stream

--- src/ch17stream/book/exercise/p08/Example.java
+++ src/ch17stream/book/exercise/p08/Example.java
@@ -2,6 +2,8 @@ package ch17stream.book.exercise.p08;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Example {
     public static void main(String[] args) {
@@ -11,13 +13,22 @@ public class Example {
                 new Member("신용권", "개발자")
         );
 
+        Map<String, List<Member>> groupingMap = list.stream()
+                .collect(Collectors.groupingBy(s -> s.getJob()));
+
+        System.out.println("[개발자 그룹핑]");
+        groupingMap.get("개발자").forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("[디자이너 그룹핑]");
+        groupingMap.get("디자이너").forEach(System.out::println);
+        System.out.println();
 
         System.out.println("[개발자]");
         list.stream()
                 .filter(x -> x.getJob().equals("개발자"))
 //                .toList()
                 .forEach(System.out::println);
-
         System.out.println();
 
         System.out.println("[디자이너]");
