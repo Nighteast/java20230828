edd63473cbd32eb8d054dd0dfdc69771c4bda002
SeungWon Jung <jung946244@naver.com>
2023-09-14 10:49:05 +0900

    9.14 exception

---
+++ src/ch11exception/lecture/C07finally.java
@@ -0,0 +1,19 @@
+package ch11exception.lecture;
+
+public class C07finally {
+    public static void main(String[] args) {
+        System.out.println("code1");
+        try {
+            int j = 3/1;
+            System.out.println("code2");
+            return;
+        } catch (ArithmeticException e) {
+            System.out.println("code3");
+        } finally {
+            System.out.println("code4");
+        }
+        System.out.println("code5");
+
+
+    }
+}
