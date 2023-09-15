7898cfce5c496832193065b17dfcc5d1692ea194
SeungWon Jung <jung946244@naver.com>
2023-09-15 14:19:46 +0900

    9.15 io stream

---
+++ src/ch18io/lecture/C13writer.java
@@ -0,0 +1,30 @@
+package ch18io.lecture;
+
+import java.io.*;
+
+public class C13writer {
+    public static void main(String[] args) {
+        // OutputStream과 비교
+        String file1 = "C:/Temp/Out5.txt";
+        String file2 = "C:/Temp/Out6.txt";
+
+        try {
+            OutputStream os = new FileOutputStream(file1);
+            Writer writer = new FileWriter(file2);
+
+            try (os; writer) {
+                os.write(65);
+                writer.write(65);
+
+                os.write(48124);        //
+                writer.write(48124);
+
+                os.flush();
+                writer.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
