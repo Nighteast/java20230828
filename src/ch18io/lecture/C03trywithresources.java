7cf3edfff4a5abe57d2a9484fd8a8eab308b86ee
SeungWon Jung <jung946244@naver.com>
2023-09-15 09:37:20 +0900

    9.15 io stream

---
+++ src/ch18io/lecture/C03trywithresources.java
@@ -0,0 +1,96 @@
+package ch18io.lecture;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class C03trywithresources {
+    public void method1() {
+        OutputStream os = null;
+        try {
+            os = new FileOutputStream("");
+            os.write(110);
+            os.write(111);
+            os.write(112);
+
+            os.close();    //io stream을 꼭 닫아야 함
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                os.close(); // io stream을 꼭 닫아야함
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void method2() {
+
+        // try-with-resources 문법
+        // 변수의 타입은 AutoCloseable이어야 함
+        try (OutputStream os = new FileOutputStream("");) {
+
+            os.write(33);
+            os.write(67);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void method3() {
+        try (
+                FileOutputStream fos = new FileOutputStream("");
+                FileInputStream fis = new FileInputStream("");
+        ) {
+
+            fos.write(234);
+            fis.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void method4() {
+        Scanner scanner = new Scanner("");
+//        scanner = new Scanner("");    // 값을 한 번만 넣었을 때 아래 문법 가능. (변수명)
+        try (scanner) {
+            scanner.hasNext();
+            scanner.next();
+        }
+
+    }
+
+    public void method5() {
+        try {
+            FileInputStream fio = new FileInputStream("");
+            FileOutputStream fos = new FileOutputStream("");
+
+            try (fio; fos;) {
+
+                fio.read();
+                fos.write(33);
+
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        }
+
+    }
+
+    public void method6() throws IOException {
+        // 안좋은 코드임...
+        FileOutputStream fos = new FileOutputStream("");
+        FileInputStream fis = new FileInputStream("");
+
+        fos.write(33);
+        fis.read();
+
+        fos.close();
+        fis.close();
+
+    }
+}
