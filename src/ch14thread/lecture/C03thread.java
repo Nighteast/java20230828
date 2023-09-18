d9c5047b7275ffb3e6f71fea73ee437f57172a4d
SeungWon Jung <jung946244@naver.com>
2023-09-18 14:58:02 +0900

    9.18 thread

---
+++ src/ch14thread/lecture/C03thread.java
@@ -0,0 +1,30 @@
+package ch14thread.lecture;
+
+import java.awt.*;
+
+public class C03thread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("띵");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+        for (int i = 0; i < 5; i++) {
+            toolkit.beep();
+            Thread.sleep(1000);
+        }
+
+    }
+}
